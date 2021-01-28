0ccf0d84c6dc366cd5109fa15be6827ed847bee0
BuildTools <anull@null.spigotmc.org>
2021-01-28 07:26:18 +0700

    Send Assignment Week1

---
+++ GI455_UnityProject/Assets/Scripts/ScriptSearch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TextSearch
+{
+    
+    public class ScriptSearch : MonoBehaviour
+    {
+        [SerializeField] string textInput;
+        [SerializeField] Text textShow;
+        public List<string> textList;
+        public GameObject inputField;
+        public GameObject showSearch;
+    
+        void Start()
+        {
+            foreach (var textShowList in textList)
+            {
+                textShow.text += textShowList + "\n";;
+            }
+        }
+    
+        public void SearchButton()
+        {
+            if (textList.Contains(textInput = inputField.GetComponent<Text>().text))
+            {
+                showSearch.GetComponent<Text>().text = $"[<color=green>{textInput}</color>] is found in data.";
+            }
+            else
+            {
+                showSearch.GetComponent<Text>().text = $"[<color=red>{textInput}</color>] is not found in data.";
+            }
+        }
+    }
+}
